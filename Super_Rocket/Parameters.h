f5aec90b7311a1ecc3e06df4eaad90a5d2040137
sgwaln <sethgwaln@gmail.com>
2025-04-30 22:58:51 -0800

    Restrucutered to be mode based, untested

--- Super_Rocket/Parameters.h
+++ Super_Rocket/Parameters.h
@@ -1,23 +1,15 @@
 #ifndef __Parameters_H
     #define __Parameters_H
 
-    #define TEST_MODE 1
+    #define PRINT_EN 1
 
-    #if TEST_MODE
-        #define ACCELL_EN 0
-        #define PRESSURE_EN 1
-        #define SERVO_EN 0
-        #define PRINT_EN 1
-    #else
-        #define ACCELL_EN 1
-        #define PRESSURE_EN 1
-        #define SERVO_EN 1
-        #define PRINT_EN 0
-    #endif
+    #define SERVO_PIN 6
 
-    // Valid Pressure Sensors MPL3115A2, BMP388
+    #define ACCEL_ADDR 0x19
+
+    // Not really sure if I like this, I thought it would be nice to support both, but I don't think this is good way to do it, I think maybe polymopryhism would be better
+    // Valid Pressure Sensors MPL3115A2, BMP388 (untested)
     #define BMP388 1
     #define MPL3115A2 2
     #define PRESSURE_SENSOR MPL3115A2
-
 #endif
